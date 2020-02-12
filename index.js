86899d5e0e4c26be053465058c6ad81c1cc9b238
khalidjambi <khalid.jambi18@gmail.com>
2020-02-12 09:29:52 +0700

    variabledasar

---
+++ index.js
@@ -0,0 +1,53 @@
+// Variable
+
+/*
+
+    -var, let, const
+    -data types
+    -scope and hoising
+
+
+*/
+
+var name = "khalid";  //declaration
+// name = "khalid"; //assigment
+let city = "Jambi";
+const age = 20;
+
+var name = "diko";
+city = "lampung";
+
+
+console.log(name , typeof name);
+console.log(city , typeof name);
+console.log(age , typeof name);
+
+// Data types
+
+/*
+
+1.primitives
+string, number, boolean , null , undefied, simbol
+
+
+2.Object
+
+
+
+*/
+
+const socialMedia = {
+    instagram: "@khalidibrahim_",
+    facebook: "khalid_ibrahim"
+};
+
+console.log(socialMedia.facebook, typeof socialMedia);
+console.log(socialMedia.instagram, typeof socialMedia);
+
+
+const hobbies = ["balapan", "mancing", "berenang"];
+console.log(hobbies [0], typeof hobbies);
+console.log(hobbies [1], typeof hobbies);
+console.log(hobbies [2], typeof hobbies);
+
+// Hosting and scopee
